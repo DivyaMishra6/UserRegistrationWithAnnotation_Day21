dc1bed64cdfcfe952537f3debb500b68a0116301
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-14 01:58:12 +0530

    Display UC1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace UserRegistrationWithAnotation
     {
         public static void Main(string[] args)
         {
+            //User Regestration
             Console.WriteLine("Welcome to User Regestration problem!!");
 
             Annotest.Display();
