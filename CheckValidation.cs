b63dc4c6903d5f4c8122b2cff7c6e1c5132fc5a0
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-14 02:03:50 +0530

    Condition hold

---
+++ CheckValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationWithAnnotationDay21
+{
+    public class CheckValidation
+    {
+        // [Required(ErrorMessage = "Property check {0} is required")]
+        [RegularExpression("^[A-Z]{1}[A-Za-z]{2,}$", ErrorMessage = "Name should be minimum 3 character and start with caps")]
+  
+        // [StringLength(100,MinimumLength =3 ,ErrorMessage = "Name should be minimum 3 character" )]
+        public string Name { get; set; }
+    }
+}
