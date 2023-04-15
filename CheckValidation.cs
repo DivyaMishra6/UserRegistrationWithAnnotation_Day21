9631a70ce58db9188ba2937a3bfb8ea1d5785463
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-15 23:35:14 +0530

    Password and Condition

--- CheckValidation.cs
+++ CheckValidation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace UserRegistrationWithAnnotationDay21
 {
     public class CheckValidation
     {
+        //FIRST NAME
         // [Required(ErrorMessage = "Property check {0} is required")]
         // [StringLength(100,MinimumLength =3 ,ErrorMessage = "Name should be minimum 3 character" )]
         [RegularExpression("^[A-Z]{1}[A-Za-z]{2,}$", ErrorMessage = "Name should be minimum 3 character and start with caps")]
@@ -16,17 +18,31 @@ namespace UserRegistrationWithAnnotationDay21
 
         public string FirstName { get; set; }
 
+
+        //LAST NAME
         [RegularExpression("^[A-Z]{1}[A-Za-z]{2,}$", ErrorMessage = "Name should be minimum 3 character and start with caps")]
         [DataType(DataType.Text)]
         public string LastName { get; set; }
 
+
+        //EMAIL
         [DataType(DataType.EmailAddress)]
         [EmailAddress]
         public string Email { get; set; }
 
+        //PHONE
+        [RegularExpression("^[1-9]{2} [0-9]{10}$", ErrorMessage = "Number must be 10 digit only with country code")]
         [DataType(DataType.PhoneNumber)]
         [Phone]
 
         public string Phone { get; set; }
+
+        //PASWORD
+        [RegularExpression("^ .{8}$", ErrorMessage = "password minimum 8 character")]
+        [DataType(DataType.Password)]
+        [PasswordPropertyText]
+
+        public string Password { get; set; }
+
     }
 }
